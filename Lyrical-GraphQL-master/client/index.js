c55c36f843abc19f78cd0fbcdcf2f18312379fe1
Asif Hasan <d.asif.hasan@gmail.com>
2018-01-09 03:03:59 -0600

    - Added mutation to create song in CreateSong component

--- Lyrical-GraphQL-master/client/index.js
+++ Lyrical-GraphQL-master/client/index.js
@@ -6,7 +6,7 @@ import { ApolloProvider } from 'react-apollo';
 
 import App from './components/App'
 import SongList from './components/SongList';
-
+import SongCreate from './components/SongCreate';
 const client = new ApolloClient({});
 
 const Root = () => {
@@ -15,6 +15,7 @@ const Root = () => {
           <Router history={hashHistory}>
               <Route path="/" component={App}>
                   <IndexRoute component={SongList}/>
+                  <Route path="song/new" component={SongCreate}/>
               </Route>
           </Router>
       </ApolloProvider>
