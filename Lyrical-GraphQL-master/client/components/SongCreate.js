762ed942d2f84c5e90ab2e1c09d6de1dd0823c28
Asif Hasan <d.asif.hasan@gmail.com>
2018-01-10 06:54:19 -0600

    - Added refetchQuery request for song list after new song is created

--- Lyrical-GraphQL-master/client/components/SongCreate.js
+++ Lyrical-GraphQL-master/client/components/SongCreate.js
@@ -5,6 +5,7 @@ import React, {Component} from 'react';
 import gql from 'graphql-tag';
 import {graphql} from 'react-apollo';
 import { Link, hashHistory} from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component{
 
@@ -18,7 +19,8 @@ class SongCreate extends Component{
         event.preventDefault();
 
         this.props.mutate({
-            variables: {title: this.state.title}
+            variables: {title: this.state.title},
+            refetchQueries: [{query: query, variables:{}}]
         }).then(() => {hashHistory.push('/')});
     }
 
