33a8b684ed43c43ae810b15425cca016fe995f5a
Asif Hasan <d.asif.hasan@gmail.com>
2018-01-10 06:34:53 -0600

    - Implemented auto navigate to home page after creating a song successfully

--- Lyrical-GraphQL-master/client/components/SongCreate.js
+++ Lyrical-GraphQL-master/client/components/SongCreate.js
@@ -4,6 +4,7 @@
 import React, {Component} from 'react';
 import gql from 'graphql-tag';
 import {graphql} from 'react-apollo';
+import { Link, hashHistory} from 'react-router';
 
 class SongCreate extends Component{
 
@@ -18,12 +19,13 @@ class SongCreate extends Component{
 
         this.props.mutate({
             variables: {title: this.state.title}
-        })
+        }).then(() => {hashHistory.push('/')});
     }
 
     render(){
         return (
             <div>
+                <Link to="/">Back</Link>
                 <h3> Create new song </h3>
                 <form onSubmit={this.onSubmit.bind(this)}>
                     <label>Song Title:</label>
