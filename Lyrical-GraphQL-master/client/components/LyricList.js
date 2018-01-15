5dba4c607072f204b9e8b8b28d8575e1b0e5771f
Asif Hasan <d.asif.hasan@gmail.com>
2018-01-15 00:08:02 -0600

    Implemented features to like lyrics and show number of likes per lyric

--- Lyrical-GraphQL-master/client/components/LyricList.js
+++ Lyrical-GraphQL-master/client/components/LyricList.js
@@ -2,31 +2,47 @@
  * Created by dasif on 1/14/2018.
  */
 import React, {Component} from 'react';
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
 
-class LyricList extends Component{
+class LyricList extends Component {
 
-    onLikeLyric(id){
-
+    onLikeLyric(id) {
+        this.props.mutate({
+            variables: {id: id}
+        })
     };
 
-    renderLyrics(){
-        return this.props.lyrics.map(({id, content}) => {
+    renderLyrics() {
+        return this.props.lyrics.map(({id, content, likes}) => {
             return (
-                <li key={id} className="collection-item" >
+                <li key={id} className="collection-item">
                     {content}
-                    <i className="material-icons" onClick={ () => this.onLikeLyric(id) }>thumb_up</i>
+                    <div className="vote-box">
+                        <i className="material-icons" onClick={ () => this.onLikeLyric(id) }>thumb_up</i>
+                        {likes}
+                    </div>
                 </li>
             );
         });
     }
 
-    render(){
+    render() {
         return (
-          <ul className="collection">
-              {this.renderLyrics()}
-          </ul>
+            <ul className="collection">
+                {this.renderLyrics()}
+            </ul>
         );
     }
 }
 
-export default LyricList;
+const mutation = gql`
+    mutation LikeLyric($id: ID!){
+        likeLyric(id: $id){
+            id
+            likes
+        }
+    }
+`;
+
+export default graphql(mutation)(LyricList);
--- Lyrical-GraphQL-master/client/queries/fetchSong.js
+++ Lyrical-GraphQL-master/client/queries/fetchSong.js
@@ -11,6 +11,7 @@ export  default gql`
             lyrics{
                 id
                 content
+                likes
             }
         }
     }
